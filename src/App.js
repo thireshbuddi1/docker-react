5683a86f6bd060a8d9ae010567128c856a5d1ef0
Thiresh Buddi <wyk4@cdc.gov>
2019-10-02 00:34:22 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React i was change
+          I was changed on feature branch
         </a>
       </header>
     </div>
